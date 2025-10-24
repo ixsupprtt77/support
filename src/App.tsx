ccd94174bcc1507026093057ff1f2d8b897b9d3d
IX Support <support@ixsupports.com>
2025-10-24 10:50:56 +0530

    Simplify deployment: Remove base path, add CNAME back, use reliable GitHub Actions workflow

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/support">
+      <BrowserRouter>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
